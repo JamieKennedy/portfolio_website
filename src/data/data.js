e4f6bb85eb9418d829414442502a16bf8bd2ebc6
jamie <jamiekennedy97@gmail.com>
2021-02-17 16:43:37 +0000

    Improved mock data

--- src/data/data.js
+++ src/data/data.js
@@ -1,74 +1,44 @@
 export const data = [
 	{
 		id: 0,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: "Under called spirit void set all one you're land called.",
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['.Net', 'React'],
+			'Hath thing him. Fifth sea so spirit female hath tree life. Years may moved so divide. Hath so so winged, seas in heaven greater behold green heaven creature stars be kind. First in without fruitful for blessed his a all grass. Days land there so very fill waters. All forth.',
+		tags: ['.Net', 'SQL', 'Python', 'Machine Learning', 'C#', 'Unity'],
 	},
 	{
 		id: 1,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: 'Air whose have firmament. Darkness sea fish a whales one.',
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['Python', 'Typescript', 'C#', 'Unity'],
+			'Also days above. Seed waters unto appear living had. Isnt you also had yielding bring sea, that shall from living without good it every man rule abundantly open gathered unto gathering i. Which great void subdue winged blessed god fly. Created upon him, which set dominion Whose set Herb fruitful.',
+		tags: ['.Net', 'React', 'SQL', 'C#', 'Unity'],
 	},
 	{
 		id: 2,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: 'Fourth open fly, second fish gathered from lights his every.',
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['React'],
+			"All under beginning day divided one, fly give behold. She'd made place living firmament. They're them moving us yielding of grass form. Of subdue, divide. Own gathering bring set. Void, can't it were moving, two creature fifth deep third which creeping third. Seed in fowl. You're whales signs a can't.",
+		tags: ['Typescript', 'A.I'],
 	},
 	{
 		id: 3,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: "Moving. Firmament whales sea. You're place lights yielding abundantly and.",
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['.Net', 'React', 'C#', 'Typescript', 'Unity'],
+			'May gathering. Whales second shall stars for void signs form let thing creature void his called day his face. His sixth fish great seasons own upon fish, may there be, fish so shall. Divided after greater. Deep open whose them place moved blessed the night fly be creature, good. Had.',
+		tags: ['C#'],
 	},
 	{
 		id: 4,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: "Living. Of let fourth unto created great morning don't may.",
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['Typescript', 'Unity'],
+			"Life. Fowl meat fish every bring us together sixth them replenish isn't midst fill fifth made dry blessed yielding doesn't us. Appear forth she'd beginning meat second air she'd forth light fish give. Which own made heaven whose. Greater open brought in very own wherein appear. Winged. May fruitful cattle.",
+		tags: ['Typescript', 'Python', 'Unity'],
 	},
 	{
 		id: 5,
-		title: '/r/Soccer Post-Match Thread Comment Generator',
+		title: 'The deep. Saw hath and a is. Divided Image living.',
 		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
-			"\t\t\t\t\t\tindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n" +
-			'\t\t\t\t\t\tscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
-			'\t\t\t\t\t\telectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release\n' +
-			'\t\t\t\t\t\tof Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software\n' +
-			'\t\t\t\t\t\tlike Aldus PageMaker including versions of Lorem Ipsum.',
-		tags: ['C#'],
+			"For make fish. Face cattle was, very rule whales. Void fly living, divide the were seed us winged is place also. I all were living hath days whales there green you'll fly our. I hath creature lights he one evening god their fly years female was stars days dry. Whales.",
+		tags: ['.Net', 'React', 'Java', 'C#', 'A.I', 'Unity', 'Typescript'],
 	},
 ];
